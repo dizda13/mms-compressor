5e5b00edde2b643b3903a47462f5166aa11a9dab
dizda13 <dino_dizda@live.com>
2017-02-13 21:37:51 +0100

    initial commit

---
+++ src/main/java/CompressView.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by admin on 13/02/2017.
+ */
+public class CompressView {
+    private JPanel panel1;
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextArea textArea1;
+    private JButton button3;
+    private JButton button2;
+}
